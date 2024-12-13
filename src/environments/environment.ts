2cbc95bd16200df94d15322415ec3ba6f48cc5dc
lareidan <nina.lareida@roche.com>
2024-12-13 22:21:47 +0100

    stuff for db

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://yscutlkarcjnnmgrviko.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzY3V0bGthcmNqbm5tZ3J2aWtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MjQ0NDIsImV4cCI6MjA0NjQwMDQ0Mn0.aIMNs_H9bLlTwmFTrjD-QVqwJCTMBHkgwgTRTJ4bAt4'
+};
